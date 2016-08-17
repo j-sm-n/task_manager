f65abce6d756e1e9abe80db4ac82282a659f42be
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-08-17 09:55:41 -0600

    more styling

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -2,6 +2,7 @@ require 'models/task_manager'
 
 class TaskManagerApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+  set :method_override, true # <= overrides the silly web browser for PUT or DELETE methods
 
   get '/' do
     erb :dashboard
@@ -21,13 +22,29 @@ class TaskManagerApp < Sinatra::Base
     redirect '/tasks'
   end
 
-  get '/tasks/:id' do |id|
-    @task = task_manager.find(id.to_i)
+  get '/tasks/:id' do
+    @task = task_manager.find(params[:id].to_i)
     erb :show
+  end
+
+  get '/tasks/:id/edit' do
+    @task = task_manager.find(params[:id].to_i)
+    erb :edit
+  end
+
+  put '/tasks/:id' do
+    task_manager.update(params[:id].to_i, params[:task])
+    redirect "/tasks/#{params[:id]}"
+  end
+
+  delete '/tasks/:id' do
+    task_manager.destroy(params[:id].to_i)
+    redirect '/tasks'
   end
 
   def task_manager
     database = YAML::Store.new('db/task_manager')
     @task_manager ||= TaskManager.new(database)
   end
+
 end
